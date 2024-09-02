0440edbfd43d4a80316ad74c4ebb8f9825bad7c7
Gogineni Pallavi Chowdary <86236414+goginenipallavichowdary@users.noreply.github.com>
2024-09-01 23:33:46 -0500

    Update Insertionsort.py

--- Insertionsort.py
+++ Insertionsort.py
@@ -1,18 +1,19 @@
 def insertionSort(num):
-	n = len(num) 
-	
-	if n <= 1:
-		return 
+    n = len(num)  
+      
+    if n <= 1:
+        return  
+ 
+    for i in range(1, n):  
+        key = num[i]  
+        j = i-1
+        while j >= 0 and key < num[j]:  
+            num[j+1] = num[j]  
+            j -= 1
+        num[j+1] = key 
+  
 
-	for i in range(1, n): 
-		  key = num[i] 
-		  j = i-1
-		  while j >= 0 and key < num[j]: 
-			num[j+1] = num[j] 
-			j -= 1
-		num[j+1] = key
-
-num = (10,9,24,12,6)
+num = [10,24,18,6,34]
 insertionSort(num)
 print(num)
 
